07527667f7fc3cbadcfafa52086119ef075b329f
Russell Garner <rgarner@zephyros-systems.co.uk>
2019-07-16 15:52:38 +0100

    Add migration to add 'opportunities'
    Keeps an original_id, an original_url, the time we tweeted it (to
    indicate intent, not hooked up) and some timestamps which will equate
    to "date scraped" and "never used" for created_at and updated_at
    respectively

---
+++ db/migrate/20190716115658_add_opportunities.rb
@@ -0,0 +1,14 @@
+class AddOpportunities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :opportunities do |t|
+      t.string :original_url, null: false
+      t.integer :original_id
+
+      t.date :tweeted_at
+      t.timestamps
+    end
+
+    add_index :opportunities, :original_url
+    add_index :opportunities, :original_id
+  end
+end
