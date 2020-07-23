956842acaa4b3caf79ce5a89cd8e9e9a1b87bf2f
Russell Garner <rgarner@zephyros-systems.co.uk>
2020-07-23 10:25:16 +0100

    Update tabular data to dt/dd
    There's a new, more HTML-idiomatic schema for key/value data
    using dt/dd. Drop the old td-based approach and update the
    specs

--- spec/dos/opportunity_spec.rb
+++ spec/dos/opportunity_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'mechanize'
 
-describe DOS::Opportunity, vcr: { cassette_name: 'fixed-list-of-opps' } do
+describe DOS::Opportunity, vcr: { cassette_name: 'fixed-list-of-opps', record: :new_episodes } do
   let(:response) { Mechanize.new.get(url) }
 
   subject(:opportunity) do
@@ -10,47 +10,39 @@ describe DOS::Opportunity, vcr: { cassette_name: 'fixed-list-of-opps' } do
 
   context 'the opportunity is an outcome' do
     let(:url) do
-      'https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/10025'
+      'https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/12757'
     end
 
     specify { expect(opportunity.type).to eql('Digital outcomes') }
-    specify { expect(opportunity.id).to eql(10025) }
-    specify { expect(opportunity.title).to eql('Design and build a website for a new local authority') }
-    specify { expect(opportunity.published).to eql(Date.new(2019, 7, 3)) }
+    specify { expect(opportunity.id).to eql(12757) }
+    specify { expect(opportunity.title).to eql('FCO Consular Intelligent Client Function - Technical Architecture & Business Analysis') }
+    specify { expect(opportunity.published).to eql(Date.new(2020, 7, 23)) }
     specify { expect(opportunity.url).to eql(url) }
-    specify { expect(opportunity.buyer).to match(/Buckinghamshire Council/) }
-    specify { expect(opportunity.location).to eql('South East England') }
-    specify { expect(opportunity.question_deadline).to eql(Date.new(2019, 7, 10)) }
-    specify { expect(opportunity.closing).to eql(Date.new(2019, 7, 17)) }
-    specify { expect(opportunity.expected_start_date).to eql(Date.new(2019, 9, 2)) }
-    specify { expect(opportunity.description).to match(/the five councils in Buckinghamshire/) }
+    specify { expect(opportunity.buyer).to eql('Foreign and Commonwealth Office (FCO)') }
+    specify { expect(opportunity.location).to eql('London') }
+    specify { expect(opportunity.question_deadline).to eql(Date.new(2020, 7, 30)) }
+    specify { expect(opportunity.closing).to eql(Date.new(2020, 8, 6)) }
+    specify { expect(opportunity.expected_start_date).to eql(Date.new(2020, 9, 14)) }
+    specify { expect(opportunity.description).to match(/Set up and embed FCO Consular's Architecture Function/) }
   end
 
   context 'the opportunity is a specialist' do
     let(:url) do
-      'https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/9969'
+      'https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/12733'
     end
 
     specify { expect(opportunity.type).to eql('Digital specialists') }
     it 'has a role' do
-      expect(opportunity.role).to eql('Developer')
+      expect(opportunity.role).to eql('Delivery manager')
     end
   end
 
   context 'the opportunity is for User Research Participants' do
     let(:url) do
-      'https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/9935'
+      'https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/12705'
     end
 
     specify { expect(opportunity.type).to eql('User research participants') }
-  end
-
-  context 'the opportunity format changed; falls over dereferencing buyer attribute, title broke' do
-    let(:url) do
-      'https://www.digitalmarketplace.service.gov.uk/digital-outcomes-and-specialists/opportunities/11438'
-    end
-
-    specify { expect(opportunity.buyer).to eql('Department for Digital, Culture, Media & Sport') }
-    specify { expect(opportunity.title).to eql('End user Compute engineer') }
+    specify { expect(opportunity.title).to eql('Energy Performance of Buildings Register - User Research Subjects') }
   end
 end
